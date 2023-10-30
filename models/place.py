cc1fb56b8c0c6643105efa0454ebe518899a7831
O-01 <sa.an-s@yandex.com>
2023-10-30 15:40:42 -0500

    Re-added changes to user, place for testing

--- models/place.py
+++ models/place.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
+import models
+from os import getenv
 from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Float, ForeignKey, Integer, String
 
 
@@ -18,3 +21,18 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     amenity_ids = []
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        reviews = relationship(
+            "Review",
+            backref="place",
+            cascade="all, delete"
+        )
+    else:
+        @property
+        def reviews(self):
+            """returns list of Review instances upon place_id"""
+            reviews_list = []
+            for obj in models.storage.all('Review').values():
+                if obj.place_id == self.id:
+                    reviews_list.append(obj)
+            return reviews_list
--- models/state.py
+++ models/state.py
@@ -14,9 +14,7 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     if getenv("HBNB_TYPE_STORAGE") == "db":
         cities = relationship(
-            "City",
-            backref="state",
-            cascade="all, delete"
+            "City", backref="state", cascade="all, delete"
         )
     else:
         @property
