6acf863c972f24dcc778636981bf5c398caf92e7
O-01 <sa.an-s@yandex.com>
2023-10-30 13:07:03 -0500

    Testing new changes

--- models/amenity.py
+++ models/amenity.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from sqlalchemy import Column, String
 from models.base_model import BaseModel
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel):
     """ Definitions for Amenity class """
-    name = ""
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship(
+        "Place",
+        secondary="place_amenity",
+        viewonly=False
+    )
