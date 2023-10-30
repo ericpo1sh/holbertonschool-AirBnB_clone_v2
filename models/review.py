119b41bd4368315137255384d66c8a47bafba48e
ericpo1sh <ericpo1sh@gmail.com>
2023-10-30 11:10:24 -0500

    Fixes to 3 files, added connection if conditional

--- models/review.py
+++ models/review.py
@@ -2,11 +2,18 @@
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from os import getenv
 
 
 class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = "reviews"
-    text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), nullable=False, ForeignKey=('place_id'))
-    user_id = Column(String(60), nullable=False, ForeignKey=('user_id'))
+
+    if getenv('HBNB_TYPE_STORAGE') == "db":
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), nullable=False, ForeignKey=('place_id'))
+        user_id = Column(String(60), nullable=False, ForeignKey=('user_id'))
+    else:
+        text = ""
+        place_id = ""
+        user_id = ""
