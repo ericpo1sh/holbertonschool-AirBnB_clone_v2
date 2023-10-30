6987e6ef9d81202c1e8b28b0dfa827a552b5226b
ericpo1sh <ericpo1sh@gmail.com>
2023-10-30 14:06:36 -0500

    This class defines a user by various attributes

--- models/review.py
+++ models/review.py
@@ -8,7 +8,7 @@ from os import getenv
 class Review(BaseModel):
     """ Review classto store review information """
     __tablename__ = "reviews"
-    if getenv('HBNB_TYPE_STOARGE') == 'db':
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         text = Column(String(1024), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
