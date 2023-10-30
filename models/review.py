b4b4e0c7cc1ea186b99ac4a3782d92739f083a48
ericpo1sh <ericpo1sh@gmail.com>
2023-10-30 12:39:29 -0500

    Changed to if statement, re-added for loop to iterate through review list

--- models/review.py
+++ models/review.py
@@ -2,18 +2,13 @@
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-from os import getenv
 
 
 class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = "reviews"
 
-    if getenv('HBNB_TYPE_STORAGE') == "db":
-        text = Column(String(1024), nullable=False)
-        place_id = Column(String(60), nullable=False, ForeignKey=('place_id'))
-        user_id = Column(String(60), nullable=False, ForeignKey=('user_id'))
-    else:
-        text = ""
-        place_id = ""
-        user_id = ""
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+
