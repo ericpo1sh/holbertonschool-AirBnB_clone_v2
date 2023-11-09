59e3d45c7a722f216ad4b134452891dacf5bbb3a
ericpo1sh <ericpo1sh@gmail.com>
2023-11-09 11:24:49 -0600

    Added HBNB prompt

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Starts Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """ Function that returns text """
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_prompt():
+    """ Function thatdisplays HBNB prompt """
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
