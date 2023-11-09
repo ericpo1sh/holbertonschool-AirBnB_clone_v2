81018534f5bf1726b110cebcf5b8f3731180b5de
ericpo1sh <ericpo1sh@gmail.com>
2023-11-09 13:03:37 -0600

    created route and function that returns Python + value of text input

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Starts Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """ Function that returns text """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_prompt():
+    """ Function thatdisplays HBNB prompt """
+    return 'HBNB'
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """ function that returns the value of text in a string """
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    """ function that returns Python + value of text input """
+    return "Python {}".format(text.replace('_', " "))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
