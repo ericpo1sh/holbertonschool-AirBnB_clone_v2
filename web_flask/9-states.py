d4bf8a7b7ffa4153f18031dc40ef06f666836790
ericpo1sh <ericpo1sh@gmail.com>
2023-11-14 16:55:08 -0600

    fix

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -67,7 +67,7 @@ def cities_by_states():
 def states():
     """ Functions that returns list of states """
     states = storage.all('State')
-    return render_template('9-states.py', states=states)
+    return render_template('8-cities_by_states.html', states=states)
 
 
 @app.route('/states/<id>', strict_slashes=False)
